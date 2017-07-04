4a059a6d0ee256101eb6703c4ebea0494942857f
afs25 <afs25@cam.ac.uk>
2017-07-04 14:15:01 +0100

    Discount info for Royal Society of Chemistry added

---
+++ macros/macros_backup/zendesk_macros_backup.py
@@ -0,0 +1,31 @@
+##Documentation: https://developer.zendesk.com/rest_api/docs/core/macros
+
+import os
+from getpass import getpass
+import json
+import csv
+
+localfolder = os.path.dirname(os.path.realpath(__file__))
+
+down = input("Would you like to dowload ticket data from Zendesk now? (y/n)")
+if down in ["Y", "y"]:
+    print("Please input your Zendesk credentials")
+    email = input("E-mail address:")
+    pword = getpass()
+    if os.name == 'nt':
+        curl = os.path.join(localfolder, "curl.exe")
+    else:
+        curl = "curl"
+    os.system(curl + " https://camacuk.zendesk.com/api/v2/macros.json -v -u " + email + ":" + pword + " > data.json")
+
+with open(os.path.join(localfolder, 'data.json')) as data_file:
+    data = json.load(data_file)
+
+with open('ticket_fields.csv', 'w') as csvfile:
+    fieldnames = ['title_in_portal', 'id', 'type', 'active', 'tag', 'url']#, 'raw_title', 'created_at', 'description', 'title', 'visible_in_portal', 'raw_description', 'required', 'updated_at', 'required_in_portal', 'collapsed_for_agents', 'regexp_for_validation', 'raw_title_in_portal', 'position', 'removable', 'editable_in_portal']
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames, extrasaction='ignore', lineterminator = '\n')
+    writer.writeheader()
+    
+    fields = data["ticket_fields"]
+    for a in fields:
+        writer.writerow(a)
